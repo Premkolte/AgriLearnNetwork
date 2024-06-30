f284e1659805cadfd1b8bc64900acac550518407
Prem Kolte <premkolte26@gmail.com>
2024-06-30 12:34:56 +0530

    Backdated commit example

--- buy_products.js
+++ buy_products.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       body.classList.add("dark-theme");
     }
 
-    themeToggle.addEventListener("click", function () {
+    themeToggle.addEventListener("click", function (){
       body.classList.toggle("dark-theme");
 
       // Save the current theme in local storage
